11e52d87362009f6525edf7a417daaf53765cb4a
My-website <mehboob@aiondigital.com>
2024-08-15 14:19:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+// window.onscroll = function() {
+//     var navbar = document.getElementById('navbar');
+
+//     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+//       navbar.classList.add('navbar-scrolled');
+//     } else {
+//       navbar.classList.remove('navbar-scrolled');
+//     }
+//   };
